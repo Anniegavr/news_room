a972d77e8d71779028ca0a7345ee6c39d273336a
Anniegavr <56108881+Anniegavr@users.noreply.github.com>
2022-05-03 00:10:24 +0300

    Delete Misplaced Directory

--- java/backend/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package backend.repository;
-
-import backend.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
